36c46fd775fc31beb6bffe4faff85175fad97f4a
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-03-22 00:17:35 +0300

    feat: add application structure layout

---
+++ src/pages/ElectronicTextbook.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const ElectronicTextbook: React.FC = () => {
+    return (
+        <div>ElectronicTextbook</div>
+    );
+};
+
+export default ElectronicTextbook;
---
+++ src/pages/MiniGames.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const MiniGames: React.FC = () => {
+    return (
+        <div>MiniGames</div>
+    );
+};
+
+export default MiniGames;
