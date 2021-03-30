f1244eb76f82e0414ed86e7f8ea253266bcdb281
Irina Selivanova <irina.selivanova1106@gmail.com>
2021-03-30 14:55:48 +0300

    RS-2:
    feat: add footer to the app

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const HeaderContainer = styled.header`
+const FooterContainer = styled.footer`
     width: 100%;
     margin: 0;
-    padding: 6px 0;
+    border-top: 2px solid #D6D6D6;
     background: url('/assets/images/header_bg.png') repeat #FFFFFF;
 `;
 
 const Container = styled.div`
     width: 80%;
-    margin: 0 auto;
+    margin: 5px auto;
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -21,9 +21,8 @@ const Logo = styled.div`
     transition: transform 0.5s ease 0s;
     &:hover {
         transform: scale(1.05);
-    }
-        
-    a {
+    }        
+    a {        
         display: block;
         width: 100%;
         height: 100%;
@@ -34,35 +33,87 @@ const Logo = styled.div`
         }
     }
 `;
-
-const Authorization = styled.button`
-    background: rgba(255, 255, 255, .2);
-    border: 2px solid rgb(10,209,189);
-    border-radius: 10px;
-    font-family: 'Bubblegum Sans',cursive,sans-serif;
-    font-size: 18px;
-    font-weight: 600;
-    color: rgb(10,209,189);
-    width: 170px;
-    height: 50px;
-    &:hover {
-        background: rgb(0,194,201);
-        color: #fff;
+const Authors = styled.div`
+    width: 100%;
+    ul {
+        margin: 0;
+        list-style: none;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-around;
     }
+    li {
+        font-family: 'BubblegumSans-Regular', cursive;
+        font-size: 22px;
+        text-transform: capitalize;
+    }
+    img {
+         width: 25px   
+    }
+    a {
+        text-decoration: none;
+        display: flex;
+        align-items: center;
+        color: #5c5c5c;
+        transition: all 0.3s ease-in-out 0s;
+        &:hover {
+            color: #9D479E;
+        }
+    }
+    
 `;
+const Copyright = styled.p`    
+    padding: 5px 0;
+    background: url('/assets/images/footer_hr.jpg') center top repeat-x;
+    text-align: center;
+    background-color: #008c99;
+    margin: 0px;
+    line-height: 35px;
+    color: #FFFFFF;
+`
+
 
 const Footer: React.FC = () => {
     return (
-        <HeaderContainer>
+        <FooterContainer>
             <Container>
                 <Logo>
                     <a href='https://rs.school/js/' target='_blank'>
                         <img src='/assets/images/logo.png' alt='logo'/>
                     </a>
                 </Logo>
-                <Authorization>Sign Up</Authorization>
+                <Authors>
+                    <ul>
+                        <li>
+                            <a href='https://github.com/vhoreho' target='_blank'>
+                                <img src='/assets/images/apprentice_boy.svg' alt='apprentice_boy'/>
+                                Vladislav Horeh
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://github.com/khusanov-95' target='_blank'>
+                                <img src='/assets/images/apprentice_boy.svg' alt='apprentice_boy'/>
+                                Farrukh Khusanov
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://github.com/Bordo951' target='_blank'>
+                                <img src='/assets/images/apprentice_boy.svg' alt='apprentice_girl'/>
+                                Irina Selivanova
+                            </a>
+                        </li>
+                        <li>
+                            <a href='https://github.com/kiryuha94' target='_blank'>
+                                <img src='/assets/images/apprentice_boy.svg' alt='apprentice_boy'/>
+                                Kiryl Pliushchenia
+                            </a>
+                        </li>
+                    </ul>
+                </Authors>
             </Container>
-        </HeaderContainer>
+            <Copyright>&copy; 2021Q1 The Rolling Scopes School, All Rights Reserved</Copyright>
+        </FooterContainer>
     );
 };
 
